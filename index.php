3585a8f95d63dd33c0eff496f3e73803bd27f4ea
Mariano Crowe <merunga@gmal.com>
2011-10-23 02:46:58 -0500

    cambio chiquito

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 /**
  *  Get Cake's root directory
  */
-	define('APP_DIR', 'app');
+	define('APP_DIR', 'app'); //
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', dirname(__FILE__));
 	define('WEBROOT_DIR', 'webroot');
